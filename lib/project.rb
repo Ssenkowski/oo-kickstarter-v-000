9e2033beeaeb647af54aa7b34f01d531f0634f77
Steven Senkowski <scsenkowski@gmail.com>
2017-11-27 23:57:26 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -4,11 +4,11 @@ attr_accessor :title, :project_backers
 
 def initialize(title)
   @title = title
-  @project_backers = []
+  @backed_projects = []
 end
 
 def add_backer(backer)
-  project_backers << backer
+  backed_projects << project_backers
 end
 
 end
